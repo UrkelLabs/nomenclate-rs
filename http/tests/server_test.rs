42a5d02e41e866d7803f07453e3548fd020b4ff2
kilpatty <seanpkilgarriff@gmail.com>
2019-04-01 00:51:24 -0500

    add server tests

--- http/tests/server_test.rs
+++ http/tests/server_test.rs
@@ -6,7 +6,32 @@ fn test_get_banner() {
 
     let banner = client.get_banner();
 
-    dbg!(banner.unwrap());
+    assert!(banner.is_ok());
+}
 
-    // assert!(banner.is_ok());
+#[test]
+fn test_get_features() {
+    let client = common::setup();
+
+    let features = client.get_features();
+
+    assert!(features.is_ok());
+}
+
+#[test]
+fn test_ping() {
+    let client = common::setup();
+
+    let ping = client.ping();
+
+    assert!(ping.is_ok());
+}
+
+#[test]
+fn test_version() {
+    let client = common::setup();
+
+    let version = client.get_version();
+
+    assert!(version.is_ok());
 }
